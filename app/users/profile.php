360612f8e89dd8a48c78370a893d3c8570a8825e
majaalin <maja_alin@hotmail.com>
2019-12-19 16:43:28 +0100

    Fixed profile updated

--- app/users/profile.php
+++ app/users/profile.php
@@ -7,3 +7,122 @@ require __DIR__.'/../autoload.php';
 if(!isset($_SESSION['user'])) {
     redirect('/');
 }
+
+$userId = $_SESSION['user']['user_id'];
+$email = $_SESSION['user']['email'];
+$username = $_SESSION['user']['username'];
+$fullName = $_SESSION['user']['full_name'];
+$avatar = $_SESSION['user']['avatar'];
+$biography = $_SESSION['user']['biography'];
+
+if (isset($_FILES['avatar'])) {
+    $avatar = $_FILES['avatar'];
+    $destination = __DIR__.'/uploads/'.date('ymd')."-".$_FILES['avatar']['name'];
+    move_uploaded_file($avatar['tmp_name'], $destination); 
+    
+    $avatarPath = date('ymd')."-".$_FILES['avatar']['name'];
+
+    $statement = $pdo->prepare("UPDATE users SET avatar = :avatar  WHERE user_id = :user_id");
+    $statement->bindParam(":avatar", $avatarPath);
+    $statement->bindParam(":user_id", $userId);
+    $statement->execute();
+};
+
+if (isset($_POST['update'])) {
+    $successes = [];
+    if (isset($_POST['email'])) {
+            $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
+    
+            $statement = $pdo->prepare('UPDATE users SET email = :email WHERE user_id = :user_id');
+            
+            if (!$statement) {
+                die(var_dump($pdo->errorInfo()));
+            }
+    
+            $statement->execute([
+                ':user_id' => $userId,
+                ':email' => $email,
+                ]);
+                
+                if ($_SESSION['user']['email'] != $email){
+
+                    $successes[] = "Your email were successfully updated";
+                
+                }
+    }
+        if (isset($_POST['username'])) {
+                $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
+        
+                $statement = $pdo->prepare('UPDATE users SET username = :username WHERE user_id = :user_id');
+                
+                if (!$statement) {
+                    die(var_dump($pdo->errorInfo()));
+                }
+        
+                $statement->execute([
+                    ':user_id' => $userId,
+                    ':username' => $username,
+                    ]);
+                    
+                    if ($_SESSION['user']['username'] != $username){
+
+                        $successes[] = "Your username were successfully updated";
+                    
+                    }
+ }
+
+        if (isset($_POST['full_name'])) {
+            $fullName = filter_var($_POST['full_name'], FILTER_SANITIZE_STRING);
+    
+            $statement = $pdo->prepare('UPDATE users SET full_name = :full_name WHERE user_id = :user_id');
+            
+            if (!$statement) {
+                die(var_dump($pdo->errorInfo()));
+            }
+    
+            $statement->execute([
+                ':user_id' => $userId,
+                ':full_name' => $fullName,
+                ]);
+                
+                if ($_SESSION['user']['full_name'] != $fullName){
+
+                    $successes[] = "Your full name were successfully updated";
+                
+                }
+    }
+
+    if (isset($_POST['biography'])) {
+        $biography = filter_var($_POST['biography'], FILTER_SANITIZE_STRING);
+
+        $statement = $pdo->prepare('UPDATE users SET biography = :biography WHERE user_id = :user_id');
+        
+        if (!$statement) {
+            die(var_dump($pdo->errorInfo()));
+        }
+
+        $statement->execute([
+            ':user_id' => $userId,
+            ':biography' => $biography,
+            ]);
+            
+            if ($_SESSION['user']['biography'] != $biography){
+
+                $successes[] = "Your biography were successfully updated";
+            
+            }
+}
+
+    if (count($successes) > 0){
+        $_SESSION['successes'] = $successes;
+        redirect('/profile.php');
+        exit;
+    } else {
+        $errors[] = "You did not update anything!";
+    }
+    if (count($errors) > 0){
+        $_SESSION['errors'] = $errors;
+        redirect('/profile.php');
+        exit;
+    }
+}
--- profile.php
+++ profile.php
@@ -3,115 +3,38 @@
 <?php 
 
 $userId = $_SESSION['user']['user_id'];
-$email = $_SESSION['user']['email'];
-$username = $_SESSION['user']['username'];
-$fullName = $_SESSION['user']['full_name'];
-$avatar = $_SESSION['user']['avatar'];
-$biography = $_SESSION['user']['biography'];
 
-if (isset($_FILES['avatar'])) {
-    $avatar = $_FILES['avatar'];
-    $destination = __DIR__.'/uploads/'.date('ymd')."-".$_FILES['avatar']['name'];
-    move_uploaded_file($avatar['tmp_name'], $destination); 
-    
-    $avatarPath = date('ymd')."-".$_FILES['avatar']['name'];
+$statement = $pdo->prepare('SELECT * FROM users WHERE user_id = :user_id');
 
-    $statement = $pdo->prepare("UPDATE users SET avatar = :avatar  WHERE user_id = :user_id");
-    $statement->bindParam(":avatar", $avatarPath);
-    $statement->bindParam(":user_id", $userId);
-    $statement->execute();
-};
+$statement->bindParam(':user_id', $userId, PDO::PARAM_STR);
 
-if (isset($_POST['update'])) {
-    $successes = [];
-    if (isset($_POST['email'])) {
-            $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-    
-            $statement = $pdo->prepare('UPDATE users SET email = :email WHERE user_id = :user_id');
-            
-            if (!$statement) {
-                die(var_dump($pdo->errorInfo()));
-            }
-    
-            $statement->execute([
-                ':user_id' => $userId,
-                ':email' => $email,
-                ]);
-                
-                $successes[] = "Your email were successfully updated";
-    }
-        if (isset($_POST['username'])) {
-                $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
-        
-                $statement = $pdo->prepare('UPDATE users SET username = :username WHERE user_id = :user_id');
-                
-                if (!$statement) {
-                    die(var_dump($pdo->errorInfo()));
-                }
-        
-                $statement->execute([
-                    ':user_id' => $userId,
-                    ':username' => $username,
-                    ]);
-                    
-                    $successes[] = "Your username were successfully updated";
- }
+$statement->execute();
 
-        if (isset($_POST['full_name'])) {
-            $fullName = filter_var($_POST['full_name'], FILTER_SANITIZE_STRING);
-    
-            $statement = $pdo->prepare('UPDATE users SET full_name = :full_name WHERE user_id = :user_id');
-            
-            if (!$statement) {
-                die(var_dump($pdo->errorInfo()));
-            }
-    
-            $statement->execute([
-                ':user_id' => $userId,
-                ':full_name' => $fullName,
-                ]);
-                
-                $successes[] = "Your full name were successfully updated";
-    }
+$user = $statement->fetch(PDO::FETCH_ASSOC);
 
-    if (isset($_POST['biography'])) {
-        $biography = filter_var($_POST['biography'], FILTER_SANITIZE_STRING);
+$avatar = $user['avatar'];
+$email = $user['email'];
+$username = $user['username'];
+$fullName = $user['full_name'];
+$biography = $user['biography'];
 
-        $statement = $pdo->prepare('UPDATE users SET biography = :biography WHERE user_id = :user_id');
-        
-        if (!$statement) {
-            die(var_dump($pdo->errorInfo()));
-        }
-
-        $statement->execute([
-            ':user_id' => $userId,
-            ':biography' => $biography,
-            ]);
-            
-            $successes[] = "Your biography were successfully updated";
-}
-
-    if (count($successes) > 0){
-        $_SESSION['successes'] = $successes;
-        redirect('/profile.php');
-        exit;
-    }
-
-
-        }
 ?>
 
 <article>
     <a href="/..">Back</a>
     <h1>Profile</h1>
     
+    <?php foreach ($errors as $error) : ?>
+        <li><?php echo $error ?></li>
+    <?php endforeach ?>
+
     <ul>
     <?php foreach ($successes as $success) : ?>
         <li><?php echo $success ?></li>
     <?php endforeach ?>
     </ul>
 
-<form action="/profile.php" method="post" enctype="multipart/form-data">
+<form action="/app/users/profile.php" method="post" enctype="multipart/form-data">
             <div>
                 <?php if (!$avatar) : ?>
                 <img src="/uploads/no-image.png" alt="">
